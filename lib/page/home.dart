56a3550eb6e653d06b1e43ac84a5fa09e875f284
roboinfo <roboinfo21@gmail.com>
2022-11-08 13:11:16 +0530

    web packages updated

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:v_choice/page/adsscreen.dart';
@@ -13,6 +14,13 @@ import 'package:flutter/services.dart';
 import 'newsscreen.dart';
 import 'notifications.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  {
+    runApp(Home());
+  };
+}
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
